b54e6f5ad75a0cb74ebfd9d027002d00f018324b
BabyShung <hao-zheng@uiowa.edu>
2014-03-27 20:58:26 -0500

    update intro

--- src/FundamentalSortings/RadixSort.java
+++ src/FundamentalSortings/RadixSort.java
@@ -1,5 +1,20 @@
 package FundamentalSortings;
 
+/**
+ * RadixSort  --- different from bucket sort
+ * 
+ * Bucket sort just scans through one time and put the current data into the corresponding bucket.
+ * After the scan it will just combine the every linked list in the bucket.
+ * 
+ * While radixsort focuses on every digit, and copy them to a new array.
+ * After process, then copy them back. And keep on repeating the same process for different digit.
+ * Technically, bucketsort is efficient than radixsort.
+ * 
+ * 
+ * 
+ * 
+ */
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,14 +29,14 @@ public class RadixSort {
 
 		while (flag) {
 			flag = false;
-			// first copy the values into buckets
+			// first copy the values into buckets array
 			for (int i = 0; i < data.length; i++) {
 				int hashIndex = (data[i] / divisor) % 10;
 				if (hashIndex > 0)
 					flag = true;
 				buckets[hashIndex].add(new Integer(data[i]));
 			}
-			// then copy the values back into vector
+			// then copy the values back into array
 			divisor *= 10;
 			int i = 0;
 			for (int j = 0; j < 10; j++) {
